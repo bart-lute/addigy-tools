80d6cdc3be55153575af34830eba381ff0737ae4
Bart Lute <bart.lute@gmail.com>
2025-09-10 13:28:47 +0200

    wtf is going on here?

--- internal/cmd/devices.go
+++ internal/cmd/devices.go
@@ -1,32 +1,32 @@
 package cmd
 
 import (
-    "github.com/bart-lute/addigy-tools/internal/pkg"
-    "github.com/spf13/cobra"
+	"github.com/bart-lute/addigy-tools/internal/pkg"
+	"github.com/spf13/cobra"
 )
 
 var (
-    devicesCmd = &cobra.Command{
-        Use:     "devices",
-        Aliases: []string{"dev"},
-        Short:   "Commands for Devices",
-    }
+	devicesCmd = &cobra.Command{
+		Use:     "devices",
+		Aliases: []string{"dev"},
+		Short:   "Commands for Devices",
+	}
 
-    // A Command to List Custom Facts
-    devicesWithSlackCmd = &cobra.Command{
-        Use:   "with-slack",
-        Short: "List of Devices with Slack installed",
-        Run:   pkg.DevicesWithSlack,
-    }
+	// A Command to List Custom Facts
+	devicesWithSlackCmd = &cobra.Command{
+		Use:   "with-slack",
+		Short: "List of Devices with Slack installed",
+		Run:   pkg.DevicesWithSlack,
+	}
 
-    // Kind of a dashboard for Werkplek Pro Devices
-    devicesSecureBootlevelCmd = &cobra.Command{
-        Use: "secure-boot-level",
-        Aliases: []string{
-            "bootlevel",
-            "sbl",
-        },
-        Short: "Secure Boot Level information for all Devices",
-        Run:   pkg.DevicesSecureBootLevel,
-    }
+	// Kind of a dashboard for Werkplek Pro Devices
+	devicesSecureBootlevelCmd = &cobra.Command{
+		Use: "secure-boot-level",
+		Aliases: []string{
+			"bootlevel",
+			"sbl",
+		},
+		Short: "Secure Boot Level information for all Devices",
+		Run:   pkg.DevicesSecureBootLevel,
+	}
 )
--- internal/pkg/devices.go
+++ internal/pkg/devices.go
@@ -1,128 +1,128 @@
 package pkg
 
 import (
-    "fmt"
-    "github.com/bart-lute/addigy-tools/internal/api"
-    "github.com/bart-lute/addigy-tools/internal/models/device_entities"
-    "github.com/jedib0t/go-pretty/v6/table"
-    "github.com/spf13/cobra"
-    "log"
-    "log/slog"
+	"fmt"
+	"github.com/bart-lute/addigy-tools/internal/api"
+	"github.com/bart-lute/addigy-tools/internal/models/device_entities"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/spf13/cobra"
+	"log"
+	"log/slog"
 )
 
 func DevicesWithSlack(cmd *cobra.Command, args []string) {
-    slog.Debug(fmt.Sprintf("Devices With Slack Installed"))
+	slog.Debug(fmt.Sprintf("Devices With Slack Installed"))
 
-    // Custom Facts use the ID as Identifier
-    slackVersionId := getKeyString("customFacts.ids.slackVersion")
-    hasSlackInstalledId := getKeyString("customFacts.ids.hasSlackInstalled")
+	// Custom Facts use the ID as Identifier
+	slackVersionId := getKeyString("customFacts.ids.slackVersion")
+	hasSlackInstalledId := getKeyString("customFacts.ids.hasSlackInstalled")
 
-    csv, err := cmd.Flags().GetBool("csv")
-    if err != nil {
-        log.Fatalln(err)
-    }
+	csv, err := cmd.Flags().GetBool("csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    desiredFactIdentifiers := []string{
-        "device_name",
-        "online",
-        "policy_id",
-        slackVersionId,
-    }
+	desiredFactIdentifiers := []string{
+		"device_name",
+		"online",
+		"policy_id",
+		slackVersionId,
+	}
 
-    var auditFilters []device_entities.AuditFilter
+	var auditFilters []device_entities.AuditFilter
 
-    auditFilters = append(auditFilters, device_entities.AuditFilter{
-        AuditField: hasSlackInstalledId,
-        Operation:  "=",
-        Type:       "boolean",
-        Value:      true,
-    })
+	auditFilters = append(auditFilters, device_entities.AuditFilter{
+		AuditField: hasSlackInstalledId,
+		Operation:  "=",
+		Type:       "boolean",
+		Value:      true,
+	})
 
-    queryFilter := device_entities.QueryFilter{
-        Filters: &auditFilters,
-    }
+	queryFilter := device_entities.QueryFilter{
+		Filters: &auditFilters,
+	}
 
-    var items []device_entities.DeviceAudit
-    api.Devices(&desiredFactIdentifiers, &queryFilter, "asc", "device_name", &items)
-    policiesMap := *getPoliciesMap()
+	var items []device_entities.DeviceAudit
+	api.Devices(&desiredFactIdentifiers, &queryFilter, "asc", "device_name", &items)
+	policiesMap := *getPoliciesMap()
 
-    tHead := table.Row{
-        "DEVICE NAME",
-        "ONLINE",
-        "POLICY ID",
-        //"LAST ONLINE",
-        "SLACK VERSION",
-    }
-    var tRows []table.Row
-    for _, item := range items {
-        tRow := table.Row{}
-        for _, i := range desiredFactIdentifiers {
-            if i == "policy_id" {
-                tRow = append(tRow, policiesMap[fmt.Sprint(item.Facts[i].Value)].Name)
-            } else {
-                tRow = append(tRow, item.Facts[i].Value)
-            }
-        }
-        tRows = append(tRows, tRow)
-    }
-    renderTable(&tHead, &tRows, csv)
+	tHead := table.Row{
+		"DEVICE NAME",
+		"ONLINE",
+		"POLICY ID",
+		//"LAST ONLINE",
+		"SLACK VERSION",
+	}
+	var tRows []table.Row
+	for _, item := range items {
+		tRow := table.Row{}
+		for _, i := range desiredFactIdentifiers {
+			if i == "policy_id" {
+				tRow = append(tRow, policiesMap[fmt.Sprint(item.Facts[i].Value)].Name)
+			} else {
+				tRow = append(tRow, item.Facts[i].Value)
+			}
+		}
+		tRows = append(tRows, tRow)
+	}
+	renderTable(&tHead, &tRows, csv)
 }
 
 // DevicesSecureBootLevel A list of devices with data related to Secure Boot Level
 func DevicesSecureBootLevel(cmd *cobra.Command, args []string) {
-    slog.Debug(fmt.Sprintf("Listing WerkPlek Pro Devices"))
+	slog.Debug(fmt.Sprintf("Listing WerkPlek Pro Devices"))
 
-    csv, err := cmd.Flags().GetBool("csv")
-    if err != nil {
-        log.Fatalln(err)
-    }
+	csv, err := cmd.Flags().GetBool("csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    desiredFactIdentifiers := []string{
-        "device_name",
-        "online",
-        "policy_id",
-        "last_online",
-        "secure_boot_level",
-        "mac_os_x_version",
-        "device_model_name",
-        "enrolled_via_dep",
-    }
+	desiredFactIdentifiers := []string{
+		"device_name",
+		"online",
+		"policy_id",
+		"last_online",
+		"secure_boot_level",
+		"mac_os_x_version",
+		"device_model_name",
+		"enrolled_via_dep",
+	}
 
-    var queryFilter device_entities.QueryFilter
+	var queryFilter device_entities.QueryFilter
 
-    var items []device_entities.DeviceAudit
-    api.Devices(&desiredFactIdentifiers, &queryFilter, "asc", "device_name", &items)
-    policiesMap := *getPoliciesMap()
+	var items []device_entities.DeviceAudit
+	api.Devices(&desiredFactIdentifiers, &queryFilter, "asc", "device_name", &items)
+	policiesMap := *getPoliciesMap()
 
-    tHead := table.Row{
-        "DEVICE NAME",
-        "ONLINE",
-        "POLICY ID",
-        "LAST ONLINE",
-        "SECURE_BOOT_LEVEL",
-        "MACOS_VERSION",
-        "DEVICE_MODEL_NAME",
-        "ENROLLED_VIA_ADE",
-    }
+	tHead := table.Row{
+		"DEVICE NAME",
+		"ONLINE",
+		"POLICY ID",
+		"LAST ONLINE",
+		"SECURE_BOOT_LEVEL",
+		"MACOS_VERSION",
+		"DEVICE_MODEL_NAME",
+		"ENROLLED_VIA_ADE",
+	}
 
-    var tRows []table.Row
-    for _, item := range items {
-        tRow := table.Row{}
-        for _, i := range desiredFactIdentifiers {
-            value := item.Facts[i].Value
-            if value == nil {
-                value = "n/a"
-            }
-            if i == "last_online" {
-                //value = item.Facts[i].Value
-                value = localDateTimeString(fmt.Sprintf("%s", item.Facts[i].Value))
-            } else if i == "policy_id" {
-                value = policiesMap[fmt.Sprint(item.Facts[i].Value)].Name
-            }
-            tRow = append(tRow, value)
-        }
-        tRows = append(tRows, tRow)
-    }
-    renderTable(&tHead, &tRows, csv)
+	var tRows []table.Row
+	for _, item := range items {
+		tRow := table.Row{}
+		for _, i := range desiredFactIdentifiers {
+			value := item.Facts[i].Value
+			if value == nil {
+				value = "n/a"
+			}
+			if i == "last_online" {
+				//value = item.Facts[i].Value
+				value = localDateTimeString(fmt.Sprintf("%s", item.Facts[i].Value))
+			} else if i == "policy_id" {
+				value = policiesMap[fmt.Sprint(item.Facts[i].Value)].Name
+			}
+			tRow = append(tRow, value)
+		}
+		tRows = append(tRows, tRow)
+	}
+	renderTable(&tHead, &tRows, csv)
 
 }
